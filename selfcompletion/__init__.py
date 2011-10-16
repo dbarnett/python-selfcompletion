77ea690be7cb1d50ef0878d03fb43dff93e3110b
David Barnett <davidbarnett2@gmail.com>
2011-10-16 16:34:05 -0700

    Create _CompletingAction and add to parser explicitly so the --_completion option shows up in help and command completion

--- selfcompletion/__init__.py
+++ selfcompletion/__init__.py
@@ -1,26 +1,37 @@
 import argparse
 import re
 
+class _CompletionAction(argparse.Action):
+    def __init__(self,
+                 option_strings,
+                 dest='_completion',
+                 metavar='CMD_LINE',
+                 default=argparse.SUPPRESS,
+                 help="Built-in command completion"):
+        super(_CompletionAction, self).__init__(
+            option_strings=option_strings,
+            dest=dest,
+            metavar=metavar,
+            default=default,
+            nargs=1,
+            type=str,
+            help=help)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        comp_words = re.split(r'\s+', values[0])
+        comp_word = comp_words[-1]
+        all_options = sum((a.option_strings for a in parser._actions), [])
+        types = [a.type for a in parser._actions if a.nargs != 0 and a.type is not None]
+        for opt in all_options:
+            if opt.startswith(comp_word):
+                print opt+' '
+        if int in types and re.match(r'^\d*$', comp_word):
+            for i in xrange(10):
+                print i
+        parser.exit()
+
 class SelfCompletingArgumentParser(argparse.ArgumentParser):
     def __init__(self, *args, **kw):
-        argparse.ArgumentParser.__init__(self, *args, **kw)
-        self._completion_parser = argparse.ArgumentParser()
-        self._completion_parser.add_argument('--_completion', type=str)
-
-    def parse_args(self, *args, **kw):
-        comp_args = self._completion_parser.parse_args(*args, **kw)
-        if comp_args._completion is not None:
-            comp_words = re.split(r'\s+', comp_args._completion)
-            comp_word = comp_words[-1]
-            all_options = sum((a.option_strings for a in self._actions), [])
-            types = [a.type for a in self._actions if a.nargs != 0 and a.type is not None]
-            for opt in all_options:
-                if opt.startswith(comp_word):
-                    print opt+' '
-            if int in types and re.match(r'^\d*$', comp_word):
-                for i in xrange(10):
-                    print i
-
-            self.exit(0)
-        else:
-            return argparse.ArgumentParser.parse_args(self, *args, **kw)
+        super(SelfCompletingArgumentParser, self).__init__(*args, **kw)
+        completion_action = _CompletionAction(['--_completion'])
+        self._add_action(completion_action)
