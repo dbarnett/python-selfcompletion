584a7f44eece3493ad23857c04033be0d264a8e3
David Barnett <davidbarnett2@gmail.com>
2012-01-22 20:16:58 -0800

    Add some tests and refactor a bit

--- selfcompletion/__init__.py
+++ selfcompletion/__init__.py
@@ -18,20 +18,30 @@ class _CompletionAction(argparse.Action):
             help=help)
 
     def __call__(self, parser, namespace, values, option_string=None):
-        comp_words = re.split(r'\s+', values)
-        comp_word = comp_words[-1]
-        all_options = sum((a.option_strings for a in parser._actions), [])
-        types = [a.type for a in parser._actions if a.nargs != 0 and a.type is not None]
-        for opt in all_options:
-            if opt.startswith(comp_word):
-                print opt+' '
-        if int in types and re.match(r'^\d*$', comp_word):
-            for i in xrange(10):
-                print i
+        comp_words = re.split(r'\s+', values.lstrip())
+        for valid_word in parser.get_valid_next_words(comp_words):
+            print valid_word
         parser.exit()
+
+class _StoreAction(argparse._StoreAction):
+    def __eq__(self, other):
+        return vars(self) == vars(other)
 
 class SelfCompletingArgumentParser(argparse.ArgumentParser):
     def __init__(self, *args, **kw):
         super(SelfCompletingArgumentParser, self).__init__(*args, **kw)
         completion_action = _CompletionAction(['--_completion'])
         self._add_action(completion_action)
+
+    def get_valid_next_words(self, words):
+        valid_words = []
+        if '--' not in words[:-1]:
+            valid_words.append('-- ')
+            for a in self._actions:
+                valid_words.extend([o+' ' for o in a.option_strings])
+        positionals = self._get_positional_actions()
+        valid_words = [w for w in valid_words if w.startswith(words[-1])]
+        if any(p.type == int for p in positionals):
+            if re.match(r'\d*$', words[-1]):
+                valid_words = ['%d'%(i,) for i in xrange(10)] + valid_words
+        return valid_words
---
+++ selfcompletion/test/test_completing.py
@@ -0,0 +1,76 @@
+try:
+    import unittest2 as unittest
+except ImportError, e:
+    import unittest
+
+import selfcompletion
+
+class TestTakesNoArgs(unittest.TestCase):
+    def setUp(self):
+        self.parser = selfcompletion.SelfCompletingArgumentParser()
+
+    def test_input_no_args(self):
+        word_options = self.parser.get_valid_next_words([''])
+        self.assertItemsEqual(word_options, [
+                '-h ', '--help ',
+                '--_completion ', '-- ',
+            ])
+
+    def test_input_single_dash(self):
+        word_options = self.parser.get_valid_next_words(['-'])
+        self.assertItemsEqual(word_options, [
+                '-h ', '--help ',
+                '--_completion ', '-- ',
+            ])
+
+    def test_input_invalid_letter(self):
+        word_options = self.parser.get_valid_next_words(['a'])
+        self.assertEqual(word_options, [])
+
+class TestTakesManyInts(unittest.TestCase):
+    def setUp(self):
+        self.parser = selfcompletion.SelfCompletingArgumentParser()
+        self.parser.add_argument('integers', metavar='N', type=int, nargs='+')
+
+    def test_input_no_args(self):
+        word_options = self.parser.get_valid_next_words([''])
+        self.assertItemsEqual(word_options, [
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                '-h ', '--help ',
+                '--_completion ', '-- ',
+            ])
+
+    def test_input_one(self):
+        word_options = self.parser.get_valid_next_words(['1'])
+        self.assertItemsEqual(word_options, [
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            ])
+
+    def test_input_single_dash(self):
+        word_options = self.parser.get_valid_next_words(['-'])
+        self.assertItemsEqual(word_options, [
+                '-h ', '--help ',
+                '--_completion ', '-- ',
+            ])
+
+    def test_input_nothing_after_dashdash(self):
+        word_options = self.parser.get_valid_next_words(['--', ''])
+        self.assertItemsEqual(word_options, [
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            ])
+
+class TestTakesManyIntsOrOptInt(unittest.TestCase):
+    def setUp(self):
+        self.parser = selfcompletion.SelfCompletingArgumentParser()
+        self.parser.add_argument('integers', metavar='N', type=int, nargs='+')
+        self.parser.add_argument('--value', type=int)
+
+    def test_input_no_args(self):
+        word_options = self.parser.get_valid_next_words([''])
+        self.assertItemsEqual(word_options, [
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                '-h ', '--help ',
+                '--value ',
+                '--_completion ', '-- ',
+            ])
+
